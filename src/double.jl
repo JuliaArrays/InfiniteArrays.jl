5bd49de97c5031548d97decf012a97f8ff56ec2b
Daan <daan.huybrechs@cs.kuleuven.be>
2018-03-20 14:25:49 +0100

    Periodic extensions

--- src/double.jl
+++ src/double.jl
@@ -1,6 +1,15 @@
 
+# generic interface
+export shift
+
+# interface for compact sequences
 export zero_padding, CompactSequence
-export leftmost, rightmost
+export support
+
+# interface for periodic extensions
+export PeriodicExtension
+export period
+
 
 """
 A `DoublyInfiniteVector` is a sequence of infinite length that can be indexed
@@ -12,21 +21,76 @@ abstract type DoublyInfiniteVector{T} <: InfVector{T}
 end
 
 
+getindex(s::DoublyInfiniteVector, r::Range) = [s[i] for i in r]
+
+# The parahermitian conjugate of a sequence is the conjugate of its reverse
+parahermitian_conjugate(s::DoublyInfiniteVector) = conj(reverse(s))
+
+adjoint(s::DoublyInfiniteVector) = conj(s)
+
+
+"The supertype of doubly infinite sequences obtained by extending finite data."
+abstract type ExtensionSequence{T} <: DoublyInfiniteVector{T}
+end
+
+"The length of the data vector that is stored in the sequence."
+data_length(s::ExtensionSequence) = length(data(s))
+
+"The data_left index of the embedded data in the sequence."
+data_leftindex(s::ExtensionSequence) = offset(s)
+"The data_right index of the embedded data in the sequence."
+data_rightindex(s::ExtensionSequence) = offset(s) + data_length(s) - 1
+"The support of the embedded data in the sequence."
+data_support(s::ExtensionSequence) = data_leftindex(s):data_rightindex(s)
+
+"The convex hull of the supports of the embedded data in two sequences."
+function data_support(s1::ExtensionSequence, s2::ExtensionSequence)
+    l = min(data_leftindex(s1), data_leftindex(s2))
+    r = max(data_rightindex(s1), data_rightindex(s2))
+    l:r
+end
+
+"Shift the sequence `k` positions to the right."
+shift(s::ExtensionSequence, k::Int) = similar(s, data(s), offset(s)+k)
+
+
+# We can do arithmetics generically by performing operations on the underlying
+# data. Concrete sequences should implement `align` which aligns the data so
+# that this is possible.
+for op in (:+, :-)
+    @eval function $op(s1::S, s2::S) where S <: ExtensionSequence
+        a, b = align(s1, s2)
+        similar(s1, $op(data(a), data(b)), offset(a))
+    end
+end
+
+for op in (:+, :-, :*, :/)
+    @eval ($op)(s::ExtensionSequence, x::Number) = similar(s, ($op)(data(s),x), offset(s))
+end
+
+for op in (:+, :-, :*)
+    @eval ($op)(x::Number, s::ExtensionSequence) = ($op)(s, x)
+end
+
+-(s::ExtensionSequence) = similar(s, -data(s), offset(s))
+
+
+
 ################
 ## Sequences with compact support, or extension by zero padding
 ################
 
 # For documentation see constructor below
-struct CompactSequence{T} <: DoublyInfiniteVector{T}
+struct CompactSequence{T} <: ExtensionSequence{T}
     # We store the non-zero coefficients as a regular vector
-    data    ::  Vector{T}
+    v       ::  Vector{T}
     # offset stores the index at which the data starts, default is 1
     offset  ::  Int
 end
 
 
 """
-    CompactSequence(data[, offset])
+    CompactSequence(v[, offset])
 
 A `CompactSequence` is a compactly supported sequence, i.e., a sequence with
 a finite number of nonzero elements. Its support is an interval ``[i,j]`` where
@@ -36,29 +100,20 @@ outside that interval.
 The `CompactSequence` stores a vector of length `j-i+1`. It can be thought of
 as an extension of this vector to doubly infinite sequences by zero padding.
 The optional `offset` in the constructor is the left endpoint `i` of the support
-of the sequence. By default, it equals the first valid index of the given vector.
+of the sequence. By default, it equals the first valid index of `v`.
 """
-CompactSequence(data::AbstractVector) = CompactSequence(data, first(eachindex(data)))
+CompactSequence(v::AbstractVector) = CompactSequence(v, first(eachindex(v)))
 
 "Extend the given vector to a doubly infinite sequency by zero padding."
-zero_padding(data::AbstractVector, optional...) = CompactSequence(data, optional...)
+zero_padding(v::AbstractVector, optional...) = CompactSequence(v, optional...)
 
-data(s::CompactSequence) = s.data
+data(s::CompactSequence) = s.v
 offset(s::CompactSequence) = s.offset
 
-"Return the leftmost index of the support of the sequence."
-leftmost(s::CompactSequence) = s.offset
-"Return the rightmost index of the support of the sequence."
-rightmost(s::CompactSequence) = s.offset + length(s.data) - 1
-
-"The length of the vector that is stored in the compact sequence."
-datalength(s::CompactSequence) = length(data(s))
+similar(s::CompactSequence, data::AbstractVector, optional...) = CompactSequence(data, optional...)
 
 "A range of indices that includes all non-zero elements of the sequence."
-support(s::CompactSequence) = leftmost(s):rightmost(s)
-
-"Shift a compact sequence `k` positions to the right."
-shift(s::CompactSequence, k::Int) = CompactSequence(data(s), offset(s)+k)
+support(s::CompactSequence) = data_support(s)
 
 # For internal use:
 # - map the index k of a sequence into an index l of the data
@@ -69,43 +124,78 @@ imapindex(s::CompactSequence, l) = l + offset(s) - 1
 
 # We override getindex to return zero outside our embedded vector.
 getindex(s::CompactSequence, k::Int) =
-    k < leftmost(s) || k > rightmost(s) ? zero(eltype(s)) : getindex(data(s), mapindex(s, k))
-
-getindex(s::CompactSequence, r::Range) = [s[i] for i in r]
-
-# Define in the future: infvector from indexing a doubly infinite sequence with an infinite range
-#getindex(s::CompactSequence, r::InfUnitRange)
+    k < data_leftindex(s) || k > data_rightindex(s) ? zero(eltype(s)) : getindex(data(s), mapindex(s, k))
 
 # Reverse the sequence in time
-reverse(s::CompactSequence) = CompactSequence(flipdim(data(s), 1), -rightmost(s))
+reverse(s::CompactSequence) = CompactSequence(flipdim(data(s), 1), -data_rightindex(s))
 
 # Take element-wise conjugates
-conj(s::CompactSequence) = CompactSequence(conj(data(s)), leftmost(s))
-
-# We define the parahermitian conjugate of a sequence as the conjugate of its reverse
-parahermitian_conjugate(s::CompactSequence) = conj(reverse(s))
+conj(s::CompactSequence) = CompactSequence(conj(data(s)), data_leftindex(s))
 
 Base.widen(s::CompactSequence) = CompactSequence(widen(data(s)), offset(s))
 
-for op in (:+, :-, :*)
-    @eval ($op)(x::Number, s::CompactSequence) = CompactSequence(($op)(s.a,x),s.offset)
-    @eval ($op)(s::CompactSequence, x::Number) = ($op)(x, s)
-end
-
-for op in (:+, :-)
-    @eval function $op(s1::CompactSequence, s2::CompactSequence)
-        l = min(leftmost(s1), leftmost(s2))
-        r = max(rightmost(s1), rightmost(s2))
-        # This is not the most efficient method in all cases, but it is generic
-        CompactSequence([s1[i] + s2[i] for i in l:r], l)
+# Return two sequences that have the same support, by zero padding
+function align(s1::CompactSequence, s2::CompactSequence)
+    if support(s1) == support(s2)
+        # they already align, do nothing
+        s1, s2
+    else
+        #they do not align, make new sequences
+        supp = data_support(s1, s2)
+        CompactSequence(s1[supp], first(supp)), CompactSequence(s2[supp], first(supp))
     end
 end
-
--(s::CompactSequence) = CompactSequence(-data(s), offset(s))
-
-/(s::CompactSequence, x::Number) = CompactSequence(data(s)/x, offset(s))
 
 
 ################
 ## Periodic extension
+################
+
+# For documentation see constructor below
+struct PeriodicExtension{T} <: ExtensionSequence{T}
+    v       ::  Vector{T}
+    offset  ::  Int
+end
+
+"""
+    PeriodicExtension(v[, offset])
+
+Extend a given vector `v` using periodicity to a doubly infinite sequence.
+
+The `offset` is optional and by default equals the first index of `v`. The sequence
+is defined by
+``
+h[i] =
+``
+"""
+PeriodicExtension(v::AbstractVector) = PeriodicExtension(v, first(eachindex(v)))
+
+data(s::PeriodicExtension) = s.v
+offset(s::PeriodicExtension) = s.offset
+
+period(s::PeriodicExtension) = data_length(s)
+
+similar(s::PeriodicExtension, data::AbstractVector, optional...) =
+    PeriodicExtension(data, optional...)
+
+# For internal use:
+# - map the index k of a sequence into an index l of the data
+mapindex(s::PeriodicExtension, k) = mod(k - offset(s), period(s)) + 1
+# - and vice-versa
+imapindex(s::PeriodicExtension, l) = l + offset(s) - 1
+
+getindex(s::PeriodicExtension, k::Int) = getindex(data(s), mapindex(s, k))
+
+# Return two periodic sequences that are aligned (have the same length and offset)
+function align(s1::PeriodicExtension, s2::PeriodicExtension)
+    @assert period(s1) == period(s2)
+    if offset(s1) == offset(s2)
+        s1, s2
+    else
+        s1, PeriodicExtension(circshift(data(s2), offset(s2)-offset(s1)), offset(s1))
+    end
+end
+
+################
+## Symmetric extensions
 ################
--- test/test_double.jl
+++ test/test_double.jl
@@ -3,6 +3,12 @@ function test_doubly_infinite_arrays()
         @testset "zero padding" begin
             test_zero_padding()
         end
+        @testset "periodic extension" begin
+            test_periodic_extension()
+        end
+        @testset "symmetric extension" begin
+            test_symmetric_extension()
+        end
     end
 end
 
@@ -12,8 +18,7 @@ function test_zero_padding()
     z1 = zero_padding(v1)
     @test eltype(z1) == eltype(v1)
     @test sum(abs.([z1[i]-v1[i] for i in eachindex(v1)])) == 0
-    @test leftmost(z1) == 1
-    @test rightmost(z1) == 3
+    @test support(z1) == 1:3
     @test z1[0] == 0
     @test z1[4] == 0
     @test z1[2:3] == v1[2:3]
@@ -25,4 +30,60 @@ function test_zero_padding()
     offset = 2
     z3 = CompactSequence(v1, offset)
     @test sum(abs.([z3[offset+i-1]-v1[i] for i in eachindex(v1)])) == 0
+    supp = InfiniteArrays.data_support(z1, z3)
+    @test first(supp) == 1
+    @test last(supp) == length(v1)+1
+
+    s = 4
+    z4 = shift(z1, s)
+    @test sum(abs.([z4[i+s]-v1[i] for i in eachindex(v1)])) == 0
+
+    # Arithmetic
+    z1 = CompactSequence(v1)
+    z2 = CompactSequence(v1, 2)
+    s1 = support(z1)
+    a = z1+z2
+    @test first(support(a)) == min(first(support(z1)), first(support(z2)))
+    @test last(support(a)) == max(last(support(z1)), last(support(z2)))
+    supp = support(a)
+    @test maximum(abs.(a[supp] - z1[supp] - z2[supp])) == 0
+    b = z1-z2
+    @test maximum(abs.(b[supp] - z1[supp] + z2[supp])) == 0
+    c = 2*z1
+    @test maximum(abs.(c[s1]-2*z1[s1])) == 0
+    d = -z1
+    @test maximum(abs.(d[s1]+z1[s1])) == 0
+    e = z1/2
+    @test maximum(abs.(e[s1]-z1[s1]/2)) <= 10eps(eltype(e))
+end
+
+function test_periodic_extension()
+    v1 = [1,2,3,4,5]
+    z1 = PeriodicExtension(v1)
+    @test eltype(z1) == eltype(v1)
+    @test sum(abs.([z1[i]-v1[i] for i in eachindex(v1)])) == 0
+    @test z1[0] == v1[end]
+    @test z1[6] == v1[1]
+    @test z1[2:3] == v1[2:3]
+
+    # Arithmetic
+    v2 = [3,4,2,1,6]
+    z1 = PeriodicExtension(v1)
+    z2 = PeriodicExtension(v2, 2)
+    p = period(z1)
+    a = z1+z2
+    @test period(a) == period(z1) == period(z2)
+    s = eachindex(v2)
+    @test maximum(abs.(a[s] - z1[s] - z2[s])) == 0
+    b = z1-z2
+    @test maximum(abs.(b[s] - z1[s] + z2[s])) == 0
+    c = 2*z1
+    @test maximum(abs.(c[s]-2*z1[s])) == 0
+    d = -z1
+    @test maximum(abs.(d[s]+z1[s])) == 0
+    e = z1/2
+    @test maximum(abs.(e[s]-z1[s]/2)) <= 10eps(eltype(e))
+end
+
+function test_symmetric_extension()
 end
