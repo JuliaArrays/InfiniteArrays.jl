8529e3bb18a5848bc56529565c8e9734e5431282
Sheehan Olver <solver@mac.com>
2020-09-09 10:25:27 +0100

    Fix a[:] for ∞ Vcat (#43)
    * Fix a[:] for ∞ Vcat
    
    * Update runtests.jl
    
    * Update Project.toml
    
    * Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -699,7 +699,7 @@ end
         @test colsupport(A,1) == 1:1
         @test Base.replace_in_print_matrix(A, 2, 1, "0") == "⋅"
         @test stringmime("text/plain", A; context=(:limit => true)) == 
-                "∞-element ApplyArray{Float64,1,typeof(vcat),Tuple{$Int,Zeros{Float64,1,Tuple{OneToInf{$Int}}}}} with indices OneToInf():\n 1.0\n  ⋅ \n  ⋅ \n  ⋅ \n  ⋅ \n  ⋅ \n  ⋅ \n  ⋅ \n  ⋅ \n  ⋅ \n ⋮"
+                "vcat($Int, ∞-element Zeros{Float64,1,Tuple{OneToInf{$Int}}} with indices OneToInf()) with indices OneToInf():\n 1.0\n  ⋅ \n  ⋅ \n  ⋅ \n  ⋅ \n  ⋅ \n  ⋅ \n  ⋅ \n  ⋅ \n  ⋅ \n ⋮"
         A = Vcat(Ones{Int}(1,∞), Diagonal(1:∞))
         @test Base.replace_in_print_matrix(A, 2, 2, "0") == "⋅"
     end
@@ -709,6 +709,26 @@ end
         @test Base.copymutable(Vcat([1.], Zeros(∞))) isa CachedArray
         @test Base.copymutable(Vcat([1.,2.], zeros(∞))) isa CachedArray
         @test Base.copymutable(Vcat(1.,2., zeros(∞))) isa CachedArray
+    end
+
+    @testset "infinite indexing" begin
+        a = Vcat(1, 1:∞)
+        @test a[:] isa Vcat
+        @test a[3:∞] ≡ 2:∞
+        @test a[3:2:∞] isa Vcat
+
+        A = Vcat(Ones(1,∞), Fill(2,1,∞))
+        @test A[:,:] == A
+        @test A[:,2:∞] isa Vcat
+
+        A = Vcat(Ones(5,5), Fill(2,∞,5))
+        @test A[:,:] == A
+        @test A[2:∞,:] isa Vcat
+
+        A = Vcat(Ones(1,∞), Fill(2,∞,∞))
+        @test A[:,:] == A
+        @test A[2:∞,2:∞] isa Vcat
+        @test A[2:∞,2:∞][1:10,1:10] == fill(2,10,10)
     end
 end
 
