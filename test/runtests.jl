d36da33d1a3dc139169a25da2afd95844c8b5cc0
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2020-11-20 11:25:39 +0000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -627,7 +627,8 @@ end
 
     @test Eye{Int}(∞) * D ≡ Eye{Int}(∞) * D ≡ D
     @test Eye(∞) * D == Eye(∞) * D == D
-    @test Eye(∞) == Eye(∞)^0 == Eye(∞)^1
+    @test Eye(∞) == Eye(∞)^0 == Eye(∞)^1 == Eye(∞)^2 == one(Eye(∞)) == copy(Eye(∞)) == one(Diagonal(Fill(2,∞)))
+    @test Diagonal(Fill(2,∞)) == copy(Diagonal(Fill(2,∞)))
 end
 
 @testset "concat" begin
