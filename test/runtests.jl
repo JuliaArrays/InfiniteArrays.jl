29e3ec234891e61fc65d768f3e1f85406cd46a9c
Sheehan Olver <solver@mac.com>
2021-02-28 21:03:19 +0000

    increase coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -54,8 +54,16 @@ import Base.Broadcast: broadcasted, Broadcasted, instantiate
 
         @test similar(a, Float64, (2,∞)) isa CachedArray{Float64}
         @test similar(a, Float64, (∞,2)) isa CachedArray{Float64}
+
+        @test similar(Array{Float64}, (∞,)) isa CachedArray{Float64}
+        @test similar(Array{Float64}, (∞,∞)) isa CachedArray{Float64}
         @test similar(Array{Float64}, (2,∞)) isa CachedArray{Float64}
         @test similar(Array{Float64}, (∞,2)) isa CachedArray{Float64}
+
+        @test similar(Array{Float64}, (oneto(∞),)) isa CachedArray{Float64}
+        @test similar(Array{Float64}, (oneto(∞),oneto(∞))) isa CachedArray{Float64}
+        @test similar(Array{Float64}, (oneto(∞),oneto(5))) isa CachedArray{Float64}
+        @test similar(Array{Float64}, (oneto(5),oneto(∞))) isa CachedArray{Float64}
     end
 
     @testset "zeros/fill/ones" begin
@@ -83,6 +91,10 @@ import Base.Broadcast: broadcasted, Broadcasted, instantiate
         @test ones(5,∞)[:,1:10] == ones(5,10)
         @test ones(∞,5)[1:10,:] == ones(10,5)
         @test ones(∞,∞)[1:5,1:5] == ones(5,5)
+
+        @test zeros(∞, 5)[1:10,:] == zeros(10,5)
+        @test zeros(Int, ∞, 5)[1:10,:] == zeros(10,5)
+        @test zeros(Int, 5, ∞)[:,1:10] == zeros(5, 10)
     end
 end
 
@@ -489,6 +501,13 @@ end
     @test @inferred(broadcast(*,Ones{Int}(∞)',D)) == @inferred(broadcast(*,D,Ones{Int}(∞)')) == D
     @test @inferred(broadcast(*,Ones(∞)',D)) == @inferred(broadcast(*,D,Ones(∞)')) == Diagonal(1.0:∞)
     @test @inferred(broadcast(*,Fill(2,∞)',D)) ≡ @inferred(broadcast(*,D,Fill(2,∞)')) ≡ 2D
+
+    @test Eye{Int}(∞, ∞) isa Eye{Int}
+    @test Eye{Int}(∞, 5) isa Eye{Int}
+    @test Eye{Int}(5, ∞) isa Eye{Int}
+    @test Eye(∞, ∞) isa Eye{Float64}
+    @test Eye(∞, 5) isa Eye{Float64}
+    @test Eye(5, ∞) isa Eye{Float64}
 
     @test Eye{Int}(∞) * D ≡ Eye{Int}(∞) * D ≡ D
     @test Eye(∞) * D == Eye(∞) * D == D
