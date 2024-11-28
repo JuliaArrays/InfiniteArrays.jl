af5528c9c937de6cc25f2df973cb4846a34d437b
Sheehan Olver <solver@mac.com>
2024-11-28 09:25:14 +0000

    avoid extension race condition

--- ext/InfiniteArraysBandedMatricesExt.jl
+++ ext/InfiniteArraysBandedMatricesExt.jl
@@ -6,13 +6,9 @@ import Base: BroadcastStyle, size, getindex, similar, copy, *, +, -, /, \, mater
 import Base.Broadcast: Broadcasted
 import InfiniteArrays: InfIndexRanges, Infinity, PosInfinity, OneToInf, InfAxes, AbstractInfUnitRange, InfRanges
 import ArrayLayouts: sub_materialize, MemoryLayout, sublayout, mulreduce, _bidiag_forwardsub!, triangularlayout, MatLdivVec
-import LazyArrays: applybroadcaststyle, applylayout, islazy, islazy_layout, simplifiable, AbstractLazyLayout, PaddedColumns, LazyArrayStyle, ApplyLayout, AbstractLazyBandedLayout
+import LazyArrays: applybroadcaststyle, applylayout, islazy, islazy_layout, simplifiable, AbstractLazyLayout, PaddedColumns, LazyArrayStyle, ApplyLayout, AbstractLazyBandedLayout, ApplyBandedLayout, BroadcastBandedLayout
 import BandedMatrices: _BandedMatrix, AbstractBandedMatrix, banded_similar, BandedMatrix, bandedcolumns, BandedColumns, bandeddata
 import FillArrays: AbstractFillMatrix, AbstractFill, getindex_value
-
-const LazyArraysBandedMatricesExt = Base.get_extension(LazyArrays, :LazyArraysBandedMatricesExt)
-const ApplyBandedLayout = LazyArraysBandedMatricesExt.ApplyBandedLayout
-const BroadcastBandedLayout = LazyArraysBandedMatricesExt.BroadcastBandedLayout
 
 BroadcastStyle(::Type{<:SubArray{<:Any,2,<:AbstractBandedMatrix,<:Tuple{<:InfIndexRanges,<:InfIndexRanges}}})= LazyArrayStyle{2}()
 _BandedMatrix(data::AbstractMatrix{T}, ::Infinity, l, u) where T = _BandedMatrix(data, ℵ₀, l, u)
