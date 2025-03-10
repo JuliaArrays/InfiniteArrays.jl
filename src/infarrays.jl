9d8e977790cbe118862c83682151f960fb015f0c
Jishnu Bhattacharya <jishnub.github@gmail.com>
2025-03-10 18:32:06 +0530

    Fix broadcaststyle for 0d FillArray

--- src/infarrays.jl
+++ src/infarrays.jl
@@ -167,7 +167,7 @@ end
 # Lazy Broadcasting
 for typ in (:Ones, :Zeros, :Fill)
     @eval begin
-        BroadcastStyle(::Type{$typ{T,N,NTuple{N,<:OneToInf}}}) where {T,N} = LazyArrayStyle{N}()
+        BroadcastStyle(::Type{$typ{T,N,<:Tuple{OneToInf,Vararg{OneToInf}}}}) where {T,N} = LazyArrayStyle{N}()
         BroadcastStyle(::Type{$typ{T,2,<:Tuple{<:Any,<:OneToInf}}}) where {T} = LazyArrayStyle{2}()
         BroadcastStyle(::Type{$typ{T,2,<:Tuple{<:OneToInf,<:Any}}}) where {T} = LazyArrayStyle{2}()
     end
