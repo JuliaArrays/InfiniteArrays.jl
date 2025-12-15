514a43357cacda68197dfdce582e1a79e6e2ce2d
Sheehan Olver <solver@mac.com>
2025-12-15 15:02:32 +0000

    bi-∞ banded matrices

--- src/biinfrange.jl
+++ src/biinfrange.jl
@@ -23,7 +23,7 @@ getindex(r::BiInfUnitRange{T}, s::AbstractUnitRange{<:Integer}) where T = conver
 
 
 
-function Base._print_matrix(io, @nospecialize(X::AbstractVecOrMat), pre, sep, post, hdots, vdots, ddots, hmod, vmod, rowsA::BiInfUnitRange, colsA)
+function Base._print_matrix(io, @nospecialize(X::AbstractVecOrMat), pre, sep, post, hdots, vdots, ddots, hmod, vmod, ::BiInfUnitRange, colsA)
     hmod, vmod = Int(hmod)::Int, Int(vmod)::Int
     ncols, idxlast = length(colsA), last(colsA)
     if !(get(io, :limit, false)::Bool)
@@ -37,7 +37,7 @@ function Base._print_matrix(io, @nospecialize(X::AbstractVecOrMat), pre, sep, po
     postsp = ""
     @assert textwidth(hdots) == textwidth(ddots)
     sepsize = length(sep)::Int
-    m, n = length(rowsA), length(colsA)
+    n = length(colsA)
     # To figure out alignments, only need to look at as many rows as could
     # fit down screen. If screen has at least as many rows as A, look at A.
     # If not, then we only need to look at the first and last chunks of A,
@@ -91,6 +91,72 @@ function Base._print_matrix(io, @nospecialize(X::AbstractVecOrMat), pre, sep, po
                 print_matrix_vdots(io, vdots, Ralign, sep, vmod, r, false)
                 print(io, i == last(rowsA) ? post : postsp * '\n')
             end
+        end
+    end
+    if isempty(rowsA)
+        print(io, pre)
+        print(io, vdots)
+        length(colsA) > 1 && print(io, "    ", ddots)
+        print(io, post)
+    end
+end
+
+
+
+function Base._print_matrix(io, @nospecialize(X::AbstractVecOrMat), pre, sep, post, hdots, vdots, ddots, hmod, vmod, ::BiInfUnitRange, ::BiInfUnitRange)
+    hmod, vmod = Int(hmod)::Int, Int(vmod)::Int
+    if !(get(io, :limit, false)::Bool)
+        screenheight = screenwidth = typemax(Int)
+    else
+        sz = displaysize(io)::Tuple{Int,Int}
+        screenheight, screenwidth = sz[1] - 4, sz[2]
+    end
+    screenwidth -= length(pre)::Int + length(post)::Int
+    presp = repeat(" ", length(pre)::Int)  # indent each row to match pre string
+    postsp = ""
+    @assert textwidth(hdots) == textwidth(ddots)
+    sepsize = length(sep)::Int
+
+    # To figure out alignments, only need to look at as many rows as could
+    # fit down screen. If screen has at least as many rows as A, look at A.
+    # If not, then we only need to look at the first and last chunks of A,
+    # each half a screen height in size.
+    quarterheight = div(screenheight,4)
+    halfheight = 2quarterheight+1
+    rowsA = -quarterheight:quarterheight
+    # Similarly for columns, only necessary to get alignments for as many
+    # columns as could conceivably fit across the screen
+
+    halfmaxpossiblecols = div(screenwidth, 4*(1+sepsize))
+    maxpossiblecols = 2halfmaxpossiblecols + 1
+    colsA = -halfmaxpossiblecols:halfmaxpossiblecols
+
+    A = alignment(io, X, rowsA, colsA, screenwidth, screenwidth, sepsize, ℵ₀)
+    c = div(screenwidth-length(hdots)::Int+1,2)+1
+    Ralign = reverse(alignment(io, X, rowsA, reverse(colsA), c, c, sepsize, ℵ₀))
+    c = screenwidth - sum(map(sum,Ralign)) - (length(Ralign)-1)*sepsize - length(hdots)::Int
+    Lalign = alignment(io, X, rowsA, colsA, c, c, sepsize, ℵ₀)
+    r = 1 # where to put dots on right half
+    
+    for i in rowsA
+        if i == rowsA[1]
+            print(io, i == first(rowsA) ? pre : presp)
+            print_matrix_vdots(io, vdots, Lalign, sep, vmod, 1, true)
+            print(io, ddots)
+            print(io, i == last(rowsA) ? post : postsp * '\n')
+        end
+
+        print(io, i == first(rowsA) ? pre : presp)
+        print_matrix_row(io, X,Lalign,i,colsA,sep,ℵ₀)
+        print(io, (i - first(rowsA)) % hmod == 0 ? hdots : repeat(" ", length(hdots)::Int))
+        # print_matrix_row(io, X,Ralign,i,colsA,sep,ℵ₀)
+        print(io, i == last(rowsA) ? post : postsp)
+        if i != rowsA[end] || i == rowsA[halfheight]; println(io); end
+        if i == rowsA[halfheight]
+            print(io, i == first(rowsA) ? pre : presp)
+            print_matrix_vdots(io, vdots, Lalign, sep, vmod, 1, true)
+            print(io, ddots)
+            print(io, i == last(rowsA) ? post : postsp * '\n')
         end
     end
     if isempty(rowsA)
